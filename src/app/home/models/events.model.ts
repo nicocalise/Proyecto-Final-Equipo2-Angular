bc7e6ed6b075462935395ca726b4bb1d2d13d6ef
nicolascalise <nicolas.calise@bootcamp-upgrade.com>
2023-03-25 12:22:16 +0100

    first interaction with node

--- src/app/home/models/events.model.ts
+++ src/app/home/models/events.model.ts
@@ -1,17 +1,29 @@
 //Example using rick and morty api will need to update with our api information.
 
-export interface CharacterInterface {
-  id: number;
+//export interface CharacterInterface {
+//  id: number;
+//  name: string;
+//  image: string;
+//}
+
+export interface EventInterface{
   name: string;
-  image: string;
+  description: string;
+  location : string;
+  date : string;
+  eventType: string;
+  capacity : string;
+  duration : string;
+  foto: string;
 }
 
-export interface CharacterResponseInterface {
-  info: {
-    count: number;
-    next: string;
-    pages: number;
-    prev: string;
-  };
-  results: CharacterInterface[];
-}
+
+//export interface CharacterResponseInterface {
+//  info: {
+//    count: number;
+//    next: string;
+//    pages: number;
+//    prev: string;
+//  };
+//  results: EventInterface[];
+//}
