21afcdcea6a8881f23c123d357be31b34021ca69
nicolascalise <nicolas.calise@bootcamp-upgrade.com>
2023-03-31 18:07:51 +0200

    event update

--- src/app/new-event/new-event.component.ts
+++ src/app/new-event/new-event.component.ts
@@ -1,6 +1,8 @@
-import { Router } from '@angular/router';
+import { ApiRequestService } from 'src/app/services/api-request.service';
+import { Router, ActivatedRoute } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { EventInterface } from '../home/models/events.model';
 
 @Component({
   selector: 'app-new-event',
@@ -8,11 +10,41 @@ import { Component } from '@angular/core';
   styleUrls: ['./new-event.component.scss']
 })
 export class NewEventComponent {
-  eventData = { name: '', description: '', location: '', date: '', eventType: '', capacity: '', duration: '',foto:'' };
 
-  constructor(private http: HttpClient, private router: Router) {}
+  public eventId: string ='';
+  public eventData:EventInterface = {
+        _id: 0, 
+        name: '', 
+        description: '', 
+        location: '', 
+        date: '', 
+        eventType: '', 
+        capacity: 0, 
+        duration: '',
+        foto:'' };
+
+  constructor(private http: HttpClient, 
+              private router: Router, 
+              private activatedRoute: ActivatedRoute,
+              private requestService:ApiRequestService) {}
+
+  ngOnInit(){
+
+    this.activatedRoute.paramMap.subscribe((params) => {
+      this.eventId = params.get('event._id') as string;
+      this.getEventId(this.eventId);
+    });
+
+  }
 
   onSubmit() {
+
+    this.activatedRoute.paramMap.subscribe((params) => {
+      this.eventId = params.get('event._id') as string;
+      //this.getEventId(this.eventId);
+    });
+debugger
+    if(this.eventId == ''){
     this.http.post<any>('http://localhost:3000/events/create', this.eventData).subscribe(
         
     (response) => {
@@ -24,7 +56,35 @@ export class NewEventComponent {
         this.router.navigate(['/home']);
       }
     );
+    } else{
+      this.http.put<any>('http://localhost:3000/events/'+this.eventData._id, 
+      {
+        name: this.eventData.name,
+        description: this.eventData.description,
+        location : this.eventData.location,
+        date : this.eventData.date,
+        eventType: this.eventData.eventType,
+        capacity : this.eventData.capacity,
+        duration: this.eventData.duration
+      }).subscribe(
+        
+      (response) => {
+          console.log(response);
+          this.router.navigate(['/home']);
+        },
+        (error) => {
+          console.log(error);
+          this.router.navigate(['/home']);
+        }
+      );
+    }
   }
 
+  private getEventId(id:string): void {
+    this.requestService.getEventID(id).subscribe(
+      (response: EventInterface) => {
+        this.eventData = response;
+      });
+  }
 }
 
