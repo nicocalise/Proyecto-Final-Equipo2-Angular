41a99697efd25c5f3838f80e8ee70c98399c90e6
DaniBootCamp <titodani@gmail.com>
2023-04-01 18:22:32 +0200

    added lazyLoad in home page, to charge events when scroll

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,8 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { CookieService } from 'ngx-cookie-service';
 import { EventInterface } from './models/events.model';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ApiRequestService } from 'src/app/services/api-request.service';
+import { InfiniteScrollModule } from 'ngx-infinite-scroll';
 
 
 @Component({
@@ -10,15 +11,28 @@ import { ApiRequestService } from 'src/app/services/api-request.service';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
+
+
 export class HomeComponent {
   eventList: EventInterface[] = [];
+  maxEvents = 100; // máximo de 100 eventos
+
+  endOfList = false;
+
   searchResults: any[] = [];
   rol?:String;
   isAdmin?:boolean;
-  
+
   constructor(private apiRequestService: ApiRequestService,private cookieService: CookieService, private http: HttpClient) {}
 
+  checkIfLastItem(lastItemId: number): boolean {
+    const lastItem = this.eventList[this.eventList.length - 1];
+    return lastItem._id === parseInt(lastItemId.toString());
+  }
+
   ngOnInit() {
+    this.loadEvents();
+
     const token = this.cookieService.get('token');
     this.rol = this.cookieService.get('rol');
     if(this.rol == 'admin'){
@@ -30,6 +44,7 @@ export class HomeComponent {
       console.log(data);
       this.eventList = data;
     });
+
   }
 
   onSearch(query: string) {
@@ -43,5 +58,28 @@ export class HomeComponent {
     });
     }
   }
+  loadEvents() {
+    this.apiRequestService.getEvents().subscribe((data: EventInterface[]) => {
+      console.log(data);
+      this.eventList = this.eventList.concat(data);
+    });
+  }
 
+  onScroll() {
+    if (this.endOfList || this.eventList.length >= this.maxEvents) {
+      return;
+    }
+
+    // Comprobar si se ha llegado al final de la lista
+    const lastItemIndex = this.eventList.length - 1;
+    const lastItemId = this.eventList[lastItemIndex]._id;
+    if (this.checkIfLastItem(lastItemId)) {
+      this.endOfList = true;
+      return;
+    }
+
+    // Si no se ha llegado al final de la lista y no se ha alcanzado el límite máximo, cargar más elementos
+
+  }
 }
+
